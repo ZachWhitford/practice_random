eade0b54b87ac73ba4828d7cdf84bf33ea3a47c1
Zachary Case Whitford <zacharycase@Zacharys-MacBook-Air.local>
2016-07-18 18:00:23 -0700

    added practice_str.cpp

---
+++ practice_str.cpp
@@ -0,0 +1,47 @@
+
+
+
+
+// C program to print all permutations with duplicates allowed
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <vector>  
+
+using namespace std; 
+
+ 
+
+
+void longest_prefix(vector<string>& str_vec){ 
+
+int shortest = str_vec[0].size(); 
+
+//find the shortest string 
+for(int i = 1; i < (int) str_vec.size(); ++i){
+
+  ((int) str[i].size() > shortest) ? (shortest = str[i].size() : shortest = shortest); 
+
+}
+
+
+
+
+for(int i = 0; i < (int) str_vec.size(); ++i){
+
+
+
+
+
+}
+
+
+
+}
+ 
+/* Driver program to test above functions */
+int main()
+{
+    string str = "ABCEFG";
+  
+}
