5494f48ab0c15a6b9ab4ec3ca3ab2c320caf08f6
Zachary Case Whitford <zacharycase@Zacharys-MacBook-Air.local>
2016-07-22 21:32:27 -0700

    added practice_str.cpp

--- practice.cpp
+++ practice.cpp
@@ -33,8 +33,7 @@ void permute(char *a, int l, int r,int &num_perm)
  }
    else
    {
-       for (i = l; i <= r; i++)
-       {
+       for (i = l; i <= r; i++){
           swap((a+l), (a+i));
           permute(a, l+1, r,num_perm);
           swap((a+l), (a+i)); //backtrack
@@ -42,8 +41,43 @@ void permute(char *a, int l, int r,int &num_perm)
    }
 }
 
+void stock_problem(int arr[], int len){
+
+vector<int> pairs; 
+
+int i = 1; 
+
+while(i != len){
+
+  while((i != len) && (arr[i-1] >= arr[i])) ++i; 
+
+  if(i == len) break; 
+
+  //cout<<i<<endl; 
+  int min = i-1; 
 
 
+  while((i != len) && (arr[i-1] <= arr[i])) ++i; 
+  //cout<<i<<endl; 
+  int max = i-1; 
+
+  pairs.push_back(min); 
+  pairs.push_back(max); 
+
+  cout<<min<<" "<<max<<endl; 
+}
+
+for(int j = 0; j < (int) pairs.size(); j+=2){
+
+  cout<<pairs[j]<<" "<<pairs[j+1]<<endl; 
+
+}
+
+
+}
+
+
+/*
 void longest_prefix(vector<string>& str_vec){ 
 
 int shortest = str_vec[0].size(); 
@@ -60,7 +94,7 @@ for(int i = 1; i < (int) str_vec.size(); ++i){
 
 
 }
- 
+ */
 /* Driver program to test above functions */
 int main()
 {
@@ -69,5 +103,9 @@ int main()
     int num_perm = 0; 
     permute(str, 0, n-1,num_perm);
     cout<<"The number of permutations was : "<<num_perm<<endl; 
-    return 0;
+    int arr[] = {100, 180, 260, 310, 40, 535, 695};
+    int arr2[] = {3,2,1}; 
+    stock_problem(arr,7); 
+    stock_problem(arr2,3); 
+
 }
